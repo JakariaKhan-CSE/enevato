a605e7df70af962b7e33384d0a38a53e41b2eb4a
Jakaria Ibna Azam Khan <mdjakariaibnaazamkhan@gmail.com>
2026-02-15 16:19:32 +0600

    After getting long time error. Alhamdulillah Fix this problem and app run successfully in android emulator.

---
+++ android/app/src/main/kotlin/com/bponi/myenvato/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.bponi.myenvato
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- android/app/src/main/kotlin/com/example/myenvato/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.myenvato
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
